db4cc3fe437cde7eee75f609c45fff3d590f6bc2
FourRam244 <104238422+FourRam244@users.noreply.github.com>
2024-02-25 15:43:59 -0600

    Delete UM_02 directory

--- UM_02/UM_02_Main.py
+++
@@ -1,9 +0,0 @@
-"""
-Programacion Modular
-"""
-
-from UM_02_Funciones import factorial, cuadrado
-resultado=factorial(5)
-resultado2=cuadrado(2)
-print(f"El factorial de 5 es {resultado}")
-print(f"El resultado {resultado2}")
